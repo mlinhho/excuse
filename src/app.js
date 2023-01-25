fc0c614f12ebfcbd4860edb30bce1c803580ba56
mlinhho <mailinhtranho8@gmail.com>
2023-01-25 00:16:14 +0000

    project

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = excusegenerator();
+  console.log(excusegenerator);
+};
+
+const excusegenerator = () => {
+  let who = ["My neighbor", "My sister", "My boyfriend", "My dog"];
+  let action = ["trashed", "lost", "stole", "ate"];
+  let what = ["my backyard", "my keys", "my food", "my shoes"];
+  let when = ["last night", "just now", "this morning", "yesterday"];
+
+  let whorandom = Math.floor(Math.random() * who.length);
+  let actionrandom = Math.floor(Math.random() * action.length);
+  let whatrandom = Math.floor(Math.random() * what.length);
+  let whenrandom = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whorandom] +
+    " " +
+    action[actionrandom] +
+    " " +
+    what[whatrandom] +
+    " " +
+    when[whenrandom] +
+    " "
+  );
 };
